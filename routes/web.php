ffdd96c4d3d65f106e15ce694fe02ce14ed6dd01
soyUnaGata <fallen.snitch@gmail.com>
2024-04-10 17:14:14 +0300

    ECOM-48 remove default files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test-connection', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Connected to the database!';
+    } catch (\Exception $e) {
+        return 'Failed to connect to the database. Error: ' . $e->getMessage();
+    }
 });
