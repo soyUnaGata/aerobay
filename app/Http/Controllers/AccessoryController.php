1ca65420c0ad8cd09709082dc3033d2ee13df34c
soyUnaGata <fallen.snitch@gmail.com>
2024-05-02 12:56:33 +0300

    ECOM-48 implement model and controller

---
+++ app/Http/Controllers/AccessoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Accessory;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AccessoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Accessory $accessory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Accessory $accessory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Accessory $accessory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Accessory $accessory)
+    {
+        //
+    }
+}
