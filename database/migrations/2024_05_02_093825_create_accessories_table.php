68b96fb223f2da669de7433b1ce9639825d1f168
soyUnaGata <fallen.snitch@gmail.com>
2024-05-03 15:41:16 +0300

    ECOM-52 set amend properties and migrated

--- database/migrations/2024_05_02_093825_create_accessories_table.php
+++ database/migrations/2024_05_02_093825_create_accessories_table.php
@@ -18,8 +18,12 @@ return new class extends Migration
             $table->float('price');
             $table->decimal('discount')->default(0.0);
             $table->string('image_url')->nullable();
+            $table->string('dimensions')->nullable();
+            $table->string('weight')->nullable();
+            $table->string('color')->nullable();
+            $table->string('type')->nullable();
             $table->integer('category_id')->default(0)->nullable();
-            $table->integer('manufacturer_id')->nullable();;
+            $table->integer('manufacturer_id')->nullable();
             $table->timestamps();
         });
     }
