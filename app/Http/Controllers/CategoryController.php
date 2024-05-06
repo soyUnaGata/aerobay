a4f7f9a4a22398fcc6e40dcad77fa7ac72deba96
soyUnaGata <fallen.snitch@gmail.com>
2024-05-06 16:32:37 +0300

    ECOM-53 implement controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Category\StoreRequest;
+use App\Http\Requests\Category\UpdateRequest;
+use App\Http\Resources\Category\CategoryResource;
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return CategoryResource::collection($categories);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        $category = Category::create($data);
+
+        return CategoryResource::make($category);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return CategoryResource::make($category);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Category $category)
+    {
+        $data = $request->validated();
+        $category->update($data);
+        return CategoryResource::make($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json(['message' => 'done'], 204);
+    }
+}
