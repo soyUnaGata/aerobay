40f1871e69964ee2cf9b0669bbbac31f41dad8e4
soyUnaGata <fallen.snitch@gmail.com>
2024-05-06 13:11:40 +0300

    ECOM-62 implement routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccessoryController;
+use App\Http\Controllers\AuthController;
 
 Route::apiResource('accessories', AccessoryController::class);
 
+Route::group([ 'middleware' => 'api', 'prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
