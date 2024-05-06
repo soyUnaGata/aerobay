e697ff6ea72d8318574d975cb038eb1af0d79d41
soyUnaGata <fallen.snitch@gmail.com>
2024-05-06 15:18:44 +0300

    ECOM-62 amend auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements JWTSubject
+class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
     public function getJWTIdentifier()
     {
--- database/migrations/2024_05_06_102134_create_users_table.php
+++ database/migrations/2024_05_06_102134_create_users_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email', 191)->unique();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
