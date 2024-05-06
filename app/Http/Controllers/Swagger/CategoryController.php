e33269fd5495a2f0fcd5c1e0ed6ee710691be4e0
soyUnaGata <fallen.snitch@gmail.com>
2024-05-06 17:32:41 +0300

    ECOM-59 implement documentation

---
+++ app/Http/Controllers/Swagger/CategoryController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * @OA\Post
+ * (
+ *      path="/api/categories",
+ *      summary="Create new category",
+ *      tags={"Category"},
+ *      @OA\RequestBody(
+ *          @OA\JsonContent(
+ *              allOf={
+ *                  @OA\Schema(
+ *                      @OA\Property(property="name", type="string", example="Scaner"),
+ *                      @OA\Property(property="description", type="string", example="Some description"),
+ *                  )
+ *              }
+ *          )
+ *      ),
+ *      @OA\Response(
+ *          response="201",
+ *          description="Category added successfully",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="data", type="object",
+ *                  @OA\Property(property="id", type="integer", example=1),
+ *                  @OA\Property(property="name", type="string", example="Scaner"),
+ *                 @OA\Property(property="description", type="string", example="Some description"),
+ *              )
+ *          )
+ *      ),
+ *      @OA\Response(response="422", description="Validation errors")
+ *  ),
+ *
+ * @OA\Get
+ *  (
+ *       path="/api/categories",
+ *       summary="List of categories",
+ *       tags={"Category"},
+ * @OA\Response(
+ *           response="201",
+ *           description="Ok",
+ *           @OA\JsonContent(
+ *               @OA\Property(property="data", type="array", @OA\Items(
+ *                   @OA\Property(property="id", type="integer", example=1),
+ *                  @OA\Property(property="name", type="string", example="Scaner"),
+ *                  @OA\Property(property="description", type="string", example="Some description"),
+ *               )
+ *              )
+ *           )
+ *       ),
+ * @OA\Response(response="422", description="Validation errors")
+ *   ),
+ *
+ * @OA\Get
+ *   (
+ *        path="/api/accessories/{category}",
+ *        summary="Individual category",
+ *        tags={"Category"},
+ *  @OA\Parameter (
+ *     description="ID of category",
+ *     in="path",
+ *     name="accessory",
+ *     required=true,
+ *     example=1
+ *  ),
+ *  @OA\Response(
+ *            response="201",
+ *            description="Ok",
+ *            @OA\JsonContent(
+ *                @OA\Property(property="data", type="array", @OA\Items(
+ *                    @OA\Property(property="id", type="integer", example=1),
+ *                   @OA\Property(property="name", type="string", example="Scaner"),
+ *                   @OA\Property(property="description", type="string", example="Some description"),
+ *                )
+ *               )
+ *            )
+ *        ),
+ * @OA\Response(response="422", description="Validation errors")
+ *    ),
+ *
+ * @OA\Patch
+ *    (
+ *         path="/api/accessories/{category}",
+ *         summary="Update category data",
+ *         tags={"Category"},
+ * @OA\Parameter (
+ *      description="ID of category",
+ *      in="path",
+ *      name="accessory",
+ *      required=true,
+ *      example=1
+ *   ),
+ *      @OA\RequestBody(
+ *           @OA\JsonContent(
+ *               allOf={
+ *                   @OA\Schema(
+ *                       @OA\Property(property="name", type="string", example="Scaner"),
+ *                       @OA\Property(property="description", type="string", example="Some description"),
+ *                   )
+ *               }
+ *           )
+ *       ),
+ * @OA\Response(
+ *             response="201",
+ *             description="Ok",
+ *             @OA\JsonContent(
+ *                 @OA\Property(property="data", type="array", @OA\Items(
+ *                     @OA\Property(property="id", type="integer", example=1),
+ *                     @OA\Property(property="name", type="string", example="Scaner"),
+ *                     @OA\Property(property="description", type="string", example="Some description"),
+ *                 )
+ *                )
+ *             )
+ *         ),
+ * @OA\Response(response="422", description="Validation errors")
+ * ),
+ *
+ * @OA\Delete
+ *   (
+ *        path="/api/accessories/{category}",
+ *        summary="Delete category",
+ *        tags={"Category"},
+ * @OA\Parameter (
+ *       description="ID of category",
+ *       in="path",
+ *       name="accessory",
+ *       required=true,
+ *       example=1
+ *    ),
+ * @OA\Response(
+ *            response="201",
+ *            description="Ok",
+ *            @OA\JsonContent(
+ *                @OA\Property(property="message", type="string", example="Done"),
+ *            )
+ *        ),
+ * @OA\Response(response="422", description="Something went wrong")
+ *    ),
+ */
+
+class CategoryController extends Controller
+{
+
+}
