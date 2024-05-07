d19ce76e093e1c2ae3c2f8c5cdc1475a137fb463
soyUnaGata <fallen.snitch@gmail.com>
2024-05-07 13:57:39 +0300

    ECOM-54 implement route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccessoryController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Swagger\ManufactoryController;
 
 Route::apiResource('accessories', AccessoryController::class);
 
 Route::apiResource('categories', CategoryController::class);
+
+Route::apiResource('manufactory', ManufactoryController::class);
