18bc2128132efb1a5614bd0dac7e68614e82c12d
soyUnaGata <fallen.snitch@gmail.com>
2024-05-08 11:25:47 +0300

    ECOM-54 implement subcategories table

---
+++ database/migrations/2024_05_07_131018_create_accessory_subcategory_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accessory_subcategory', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('accessory_id');
+            $table->unsignedBigInteger('subcategory_id');
+
+            $table->foreign('accessory_id')->references('id')->on('accessories')->onDelete('cascade');
+            $table->foreign('subcategory_id')->references('id')->on('subcategories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accessory_subcategory');
+    }
+};
